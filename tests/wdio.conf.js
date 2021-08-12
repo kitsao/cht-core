d24654110e35b7027050db947caa29f3584b6b46
ngaruko <bede.ngaruko@gmail.com>
2021-08-12 17:03:25 +1200

    increase timeout

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -158,7 +158,7 @@ const baseConfig = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: 'bdd',
-    timeout: 700000, // login as offline takes forever ATM
+    timeout: 1200000, // login as offline takes forever ATM
   },
   //
   // =====
