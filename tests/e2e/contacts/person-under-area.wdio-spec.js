312d022939337fc6f82e79fa778185da8e0cb40b
m5r <mokht@rmi.al>
2021-08-16 13:24:35 +0800

    resolve conflict

--- tests/e2e/contacts/person-under-area.wdio-spec.js
+++ tests/e2e/contacts/person-under-area.wdio-spec.js
@@ -59,7 +59,7 @@ describe('Create Person Under Area', async () => {
     await usersAdminPage.inputAddUserFields(username, 'Jack', 'CHW', healthCenter2.name, person2.name, password);
     await usersAdminPage.saveUser();
     await usersAdminPage.logout();
-    await loginPage.login(username, password);
+    await loginPage.login({ username, password });
     await commonPage.closeTour();
 
     await commonPage.goToPeople();
