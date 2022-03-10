b3443e3bd6eca8af1b99a97fbbd05da14124d88f
Derick Lung'aho <83162+derickl@users.noreply.github.com>
2022-03-10 13:57:50 +0300

    wip

--- gh-actions/dynamic-rapidpro-workspace-settings/index.js
+++ gh-actions/dynamic-rapidpro-workspace-settings/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const replace = require('replace-in-file');
+const template = require('template-file')
 const path = require('path');
 const axios = require('axios').default;
 const fs = require('fs');
@@ -10,9 +11,9 @@ const search = (haystack, needle) =>
   needle in haystack
     ? haystack[needle]
     : Object.values(haystack).reduce((acc, val) => {
-        if (acc !== undefined) return acc;
-        if (typeof val === 'object') return search(val, needle);
-      }, undefined);
+      if (acc !== undefined) return acc;
+      if (typeof val === 'object') return search(val, needle);
+    }, undefined);
 
 const regex = expr => new RegExp(expr, 'g');
 
@@ -30,11 +31,24 @@ const getCouchDbUrl = (value_key) => {
   const couch_username = core.getInput('couch_username');
   const couch_password = core.getInput('couch_password');
   const hostname = core.getInput('hostname');
-  const couch_node_name = core.getInput('couch_node_name'); 
+  const couch_node_name = core.getInput('couch_node_name');
   const url = new URL(`https://${hostname}/_node/${couch_node_name}/_config/medic-credentials/${value_key}`);
   url.username = couch_username;
   url.password = couch_password;
   return url;
+};
+
+const fields = ['rp_hostname', 'value_key', 'rp_contact_group', 'write_patient_state_flow', 'rp_api_token', 'rp_flows', 'directory']
+
+const inputs = {
+  'rp_hostname': 'blah_value'
+}
+const getInputs = (core) => {
+  const result = {};
+  fields.forEach((field) => {
+    result[field] = core.getInput(field);
+  });
+  return result;
 };
 
 async function run() {
@@ -43,40 +57,43 @@ async function run() {
     if (!githubWorkspacePath) {
       throw new Error('GITHUB_WORKSPACE not defined');
     }
-    const rp_hostname = core.getInput('rp_hostname');
-    const value_key = core.getInput('value_key');
-    const rp_contact_group = core.getInput('rp_contact_group');
-    const write_patient_state_flow = core.getInput('write_patient_state_flow');  
-    const rp_api_token = core.getInput('rp_api_token');
-    const rp_flows = core.getInput('rp_flows');
-    const codeRepository = path.resolve(path.resolve(githubWorkspacePath), core.getInput('directory'));
+
+    const context = getInputs(core);
+    const codeRepository = path.resolve(path.resolve(githubWorkspacePath), context.directory);
     process.chdir(codeRepository);
     const appSettings = require(`${codeRepository}/app_settings.json`);
     const flowsFileName = `${codeRepository}/flows.js`;
     // console.log(appSettings);
+
+    const replacedString = await template.render('file-name', {
+      'base_url': 'base_url_value',
+      ...
+    })
+
+    await fs.write('file-name', replacedString);
     const options = {
       files: `${codeRepository}/app_settings.json`,
       from: [
-        regex(search(appSettings, 'base_url')), 
-        regex(search(appSettings, 'value_key')), 
-        // search(appSettings, 'groups').expr, 
+        regex(search(appSettings, 'base_url')),
+        regex(search(appSettings, 'value_key')),
+        // search(appSettings, 'groups').expr,
         // regex(search(appSettings, 'flow').expr)
       ],
       to: [
-        rp_hostname, value_key, 
-        `['${rp_contact_group}']`, 
-        `'${write_patient_state_flow}'`
+        context.rp_hostname, context.value_key,
+        `['${context.rp_contact_group}']`,
+        `'${context.write_patient_state_flow}'`
       ]
     };
-    
-    const flowsContent = `module.exports = ${rp_flows};`;
 
-    setMedicCredentials(getCouchDbUrl(value_key), rp_api_token);
+    const flowsContent = `module.exports = ${context.rp_flows};`;
+
+    setMedicCredentials(getCouchDbUrl(context.value_key), context.rp_api_token);
     replace(options);
     writeFlowsFile(flowsFileName, flowsContent);
 
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${payload}`);  
+    console.log(`The event payload: ${payload}`); //eslint-disable no-console
   } catch (error) {
     core.setFailed(error.message);
   }
